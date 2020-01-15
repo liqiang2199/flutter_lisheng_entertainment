88ed52fbd1f8465882ded5377efb80ccac4e98cc
liqiang2199 <1094655541@qq.com>
2020-01-15 18:16:24 +0800

    登录接口对接

---
+++ lib/model/http/vietnam_hanoi/hanoi_trend/HanoiOneLotteryMoreTrendHttpBeen.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'HanoiOneLotteryMoreTrendHttpBeen.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+HanoiOneLotteryMoreTrendHttpBeen _$HanoiOneLotteryMoreTrendHttpBeenFromJson(
+    Map<String, dynamic> json) {
+  return HanoiOneLotteryMoreTrendHttpBeen()
+    ..token = json['token'] as String
+    ..limmit = json['limmit'] as String
+    ..address = json['address'] as String;
+}
+
+Map<String, dynamic> _$HanoiOneLotteryMoreTrendHttpBeenToJson(
+        HanoiOneLotteryMoreTrendHttpBeen instance) =>
+    <String, dynamic>{
+      'token': instance.token,
+      'limmit': instance.limmit,
+      'address': instance.address,
+    };
