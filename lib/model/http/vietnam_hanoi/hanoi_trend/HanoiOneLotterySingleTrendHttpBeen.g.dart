3c0a69719404e747c852a86c9dc4ebe27546f3ca
liqiang <1094655541@qq.com>
2020-01-16 09:07:11 +0800

    一分彩修改

---
+++ lib/model/http/vietnam_hanoi/hanoi_trend/HanoiOneLotterySingleTrendHttpBeen.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'HanoiOneLotterySingleTrendHttpBeen.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+HanoiOneLotterySingleTrendHttpBeen _$HanoiOneLotterySingleTrendHttpBeenFromJson(
+    Map<String, dynamic> json) {
+  return HanoiOneLotterySingleTrendHttpBeen()
+    ..token = json['token'] as String
+    ..limmit = json['limmit'] as String
+    ..address = json['address'] as String;
+}
+
+Map<String, dynamic> _$HanoiOneLotterySingleTrendHttpBeenToJson(
+        HanoiOneLotterySingleTrendHttpBeen instance) =>
+    <String, dynamic>{
+      'token': instance.token,
+      'limmit': instance.limmit,
+      'address': instance.address,
+    };
