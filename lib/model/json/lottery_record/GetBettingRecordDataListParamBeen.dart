5a2db3a3b2656a5a33d981919e117af6ad15b216
liqiang <1094655541@qq.com>
2020-03-11 15:35:21 +0800

    修改彩种

--- lib/model/json/lottery_record/GetBettingRecordDataListParamBeen.dart
+++ lib/model/json/lottery_record/GetBettingRecordDataListParamBeen.dart
@@ -10,7 +10,13 @@ class GetBettingRecordDataListParamBeen {
   String three_num;
   String four_num;
   String five_num;
+  String six_num;//幸运飞艇
+  String seven_num;//幸运飞艇
+  String eight_num;//幸运飞艇
+  String nine_num;//幸运飞艇
+  String ten_num;//幸运飞艇
   String data_num;
+  String num_type;//幸运飞艇
 
   GetBettingRecordDataListParamBeen(this.one_num, this.two_num, this.three_num,
       this.data_num);
