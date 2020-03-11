d792a3f06ba73651b6e131dd5ec72a2159956981
liqiang <1094655541@qq.com>
2020-03-11 15:44:14 +0800

    修改彩种

---
+++ lib/model/http/CommonUploadHttpBeen.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'CommonUploadHttpBeen.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CommonUploadHttpBeen _$CommonUploadHttpBeenFromJson(Map<String, dynamic> json) {
+  return CommonUploadHttpBeen(
+    json['file'] as String,
+  );
+}
+
+Map<String, dynamic> _$CommonUploadHttpBeenToJson(
+        CommonUploadHttpBeen instance) =>
+    <String, dynamic>{
+      'file': instance.file,
+    };
---
+++ lib/model/http/order/DelOrderHttpBeen.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'DelOrderHttpBeen.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DelOrderHttpBeen _$DelOrderHttpBeenFromJson(Map<String, dynamic> json) {
+  return DelOrderHttpBeen()
+    ..token = json['token'] as String
+    ..id = json['id'] as String;
+}
+
+Map<String, dynamic> _$DelOrderHttpBeenToJson(DelOrderHttpBeen instance) =>
+    <String, dynamic>{
+      'token': instance.token,
+      'id': instance.id,
+    };
